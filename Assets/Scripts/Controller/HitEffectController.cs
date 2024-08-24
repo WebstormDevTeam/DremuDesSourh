3f322d8e7b48294b536818def61d401b79baf34b
Ice-Soul <18600213269@163.com>
2024-08-24 21:56:34 +0800

    我不用其他人的制谱器啦！主策！

--- Assets/Scripts/Controller/HitEffectController.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-namespace Controller
-{
-    public class HitEffectController : MonoBehaviour
-    {
-        public SpriteRenderer spriteRenderer;//渲染器
-        public Animator animator;//动画器
-    }
-}
--- Assets/Scripts/Form/BPMList/Apply.cs
+++
@@ -1,8 +0,0 @@
-using Scenes.PublicScripts;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Apply : PublicButton
-{
-}
