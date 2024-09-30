65a7a1ecfeb7e1d8f09c6da7c8d520fe3f2de35b
webstorm-dxy <dxy20070515@163.com>
2024-09-30 20:12:46 +0800

    回滚

--- Assets/ChartCreator/Scripts/Maincontroller.cs
+++ Assets/ChartCreator/Scripts/Maincontroller.cs
@@ -1,9 +1,13 @@
+using System;
 using UnityEngine;
 
 namespace ChartCreator.Scripts
 {
     public class Maincontroller : MonoBehaviour
     {
-        
+        private void Start()
+        {
+
+        }
     }
 }
